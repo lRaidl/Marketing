d8e54459bbe39e3f722d6eb29d37aba786cb3add
Nicolas Castellanos <mj4nico@gmail.com>
2022-03-25 21:14:46 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js" ></script>
+    <link rel="stylesheet" href="assets/css/line-awesome-font-awesome.min.css">
+    <title>Pagina Bonita</title>
+
+</head>
+<body>
+    
+    <div id="main-content">
+    
+        <!-- Carousel Inicial -->
+        <!-- <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class='tra'>
+                        <h1 style='margin-left:30px'>TITULO CHIMBA</h1>
+                        <h6>Descipcion chimba muy muy muy chimba</h6>
+                    </div>
+                    <img src="assets/image/image1.jpg" class="portada" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <div class='tra'>
+                        <h1 style='margin-left:10px'>TITULO CHIMBA</h1>
+                        <h6>Descipcion chimba muy muy muy chimba</h6>
+                    </div>
+                    <img src="assets/image/image2.jpg" class="portada" alt="...">
+                </div>
+            </div>
+        </div> -->
+        <!-- Conocenos -->
+        <div id='fc'>
+            <img id='lgi' src="assets/image/logo.png" alt="">
+            <div id="tfc">
+                <div class="ttc">
+                    <span>No pierdas la mejor oportunidad</span>
+                </div>
+                <div class="tmc">
+                    <span>PARA SALIR DE POBRE</span>
+                </div>
+                <div class="tbc">
+                    <span>Es una promesa falsa no nos hacemos responsables por falsas ilusiones puto</span>
+                </div>
+            </div>
+            <button class="bts"><a class='bta' href='#conocenos'>Empieza Ahora</a></button>
+                
+        </div>
+        <!-- <div id="menu">
+            <a id='op'  href="">Inicio</a>
+            <a id='op'  href="#conocenos">Conocenos</a>
+            <a id='op'  href="">Mision Y Vision</a>
+            <a id='op'  href="">Contactanos</a>
+        </div> -->
+        <div id="conocenos">
+            <div class="ctx">
+                <peq>¿Quienes somos?</peq>
+                <br>
+                <init>TU AGENCIA DE MARKETING</init>
+                <i class="las la-dog"></i>
+                <br>
+                <text>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</text>
+            </div>
+            <br>
+            <div class="ccd">
+                <div class="card" style="width: 18rem;">
+                    <img style='max-height:380px' class="card-img-top" src="assets/image/perso2.jpeg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <img style='max-height:380px' class="card-img-top" src="assets/image/perso1.jpeg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <img style='max-height:380px' class="card-img-top" src="assets/image/perso3.jpeg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="salt1">
+            <div class='img-content'><img class='icon-m' src="assets/svg/like.png" alt=""><span>25043</span><otext>CLIENTES FELICES</otext></div>
+            <div class='img-content'><img class='icon-m' src="assets/svg/hand.png" alt=""><span>45645</span><otext>RICOS NUEVOS</otext></div>
+            <div class='img-content'><img class='icon-m' src="assets/svg/people.png" alt=""><span>9312</span><otext>EMPLEOS GENERADOS</otext></div>
+            <div class='img-content'><img class='icon-m' src="assets/svg/worldwide.png" alt=""><span>1345</span><otext>OFICIONA GLOBALES</otext></div>
+            
+            
+            
+        </div>
+    </div>
+</div>
+</body>
+</html>
